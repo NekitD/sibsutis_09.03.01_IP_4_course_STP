0ce787dbb470e2574a641493d5018b8ad4d1072f
EL0re <griazinaleksey@gmail.com>
2026-09-05 15:25:13 +0700

    2 step

--- лаб1/test.cpp
+++ лаб1/test.cpp
@@ -5,24 +5,34 @@ using namespace std;
 
 int main()
 {
-    // C0 C1
-    // applyDiscount
+    //C0 C1 C2
     cout << "applyDiscount(-1, true) = " << applyDiscount(-1, true) << endl;
     cout << "applyDiscount(0, true) = " << applyDiscount(0, true) << endl; 
     cout << "applyDiscount(10001, true) = " << applyDiscount(10001, true) << endl;
     cout << "applyDiscount(10001, false) = " << applyDiscount(10001, false) << endl;
     cout << "applyDiscount(6000, true) = " << applyDiscount(6000, true) << endl; 
     cout << "applyDiscount(6000, false) = " << applyDiscount(6000, false) << endl; 
-
-    // calcShipping
+    //C0 C1 C2
     cout << "calcShipping(-1) = " << calcShipping(-1) << endl;
     cout << "calcShipping(1) = " << calcShipping(1) << endl;
     cout << "calcShipping(6000) = " << calcShipping(6000) << endl;
-
-    // finalPrice
+    //C0 С1 C2
     cout << "finalPrice(-1, 1, 1) = " << finalPrice(-1, 1, 1) << endl;
     cout << "finalPrice(1, -1, 1) = " << finalPrice(1, -1, 1) << endl;
     cout << "finalPrice(1, 1, -1) = " << finalPrice(1, 1, -1) << endl;
     cout << "finalPrice(1, 1000, 1) = " << finalPrice(1000, 0, 0) << endl;
     cout << "finalPrice(1, 1000, 1) = " << finalPrice(1, 1000, 1) << endl;
+    //C0
+    cout << "countExpensiveItems({}, 10.0) = " << countExpensiveItems({}, 10.0);
+    cout << "countExpensiveItems({15.0, 20.0}, 10.0) = " << countExpensiveItems({15.0, 20.0}, 10.0);
+    cout << "countExpensiveItems({5.0, 3.0}, 10.0) = " << countExpensiveItems({5.0, 3.0}, 10.0);
+
+    //C1
+    cout << "countExpensiveItems({5.0, 15.0}, 10.0) = " << countExpensiveItems({5.0, 15.0}, 10.0);
+
+    //C2
+    cout << "countExpensiveItems({5.0, 10.0, 15.0}, 15.0) = " << countExpensiveItems({5.0, 10.0, 15.0}, 15.0);
+    cout << "countExpensiveItems({5.0, 10.0, 15.0}, 10.0) = " << countExpensiveItems({5.0, 10.0, 15.0}, 10.0);
 }
+
+
