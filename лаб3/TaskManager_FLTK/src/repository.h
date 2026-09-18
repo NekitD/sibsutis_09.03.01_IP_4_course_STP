f587d87777afc9791498925bc7feaba5a2a55431
NekitD <nilitadorogin@gmail.com>
2026-09-18 18:13:39 +0700

    Add repository.h

--- лаб3/TaskManager_FLTK/src/repository.h
+++ лаб3/TaskManager_FLTK/src/repository.h
@@ -10,12 +10,24 @@ private:
     std::vector<T> items;
 
 public:
-    void Add(T item);
-    void Remove(int index);
-    void Update(int index, T item);
-    int Size();
-    std::vector<T> GetAll();
-    void Clear();
+    void Add(T item) {
+        items.push_back(item);
+    }
+    void Remove(int index) {
+        items.erase(index);
+    }
+    void Update(int index, T item) {
+        items[index] = item;
+    }
+    int Size() const{
+        return items.size();
+    }
+    std::vector<T> GetAll() const{
+        return items;
+    }
+    void Clear() {
+        items.clear();
+    }
 };
 
 #endif
